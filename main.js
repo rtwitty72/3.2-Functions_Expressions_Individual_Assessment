250ccd69eed00b3b31b76e1ee1952cef5576d92d
MacBook Pro <macbookpro@MacBooks-MBP-2.attlocal.net>
2017-06-14 00:41:28 -0400

    First Submittal

---
+++ main.js
@@ -0,0 +1,106 @@
+// 1.
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// A:
+
+function max(){
+    // Your answer here
+}if (largest) {
+  return
+} else {
+
+}
+
+
+// 2.
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// A:
+
+function maxOfThree(){
+    // Your answer here
+}
+
+
+// 3.
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// A:
+
+function isVowel(char){
+  // Your answer here
+}
+
+
+// 4.
+// Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
+// A:
+  function sum(a, b) {
+     return (a + b);
+
+}
+sum (2, 4);
+
+
+
+
+
+// 5.
+// Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
+// A:
+function avg(item1, item2, item3){
+return (item1 + item2 + item3)/3;
+
+}
+
+avg(1, 2, 3);
+
+
+
+// 6.
+// Write a function called `getLength` that takes one parameter (a string) and returns the length
+// A:
+function getLength() {
+  var string = ("Hello World");
+  return string.length;
+}
+getLength().
+
+// 7.
+// Write a function called `greaterThan` that takes two parameters
+// and returns `true` if the second parameter is greater than the first.
+// Otherwise the function should return `false`.
+// A:
+function greaterThan(item1, item2); {
+  var Item1 = 6;
+  var Item2 = 3;
+  if (Item2 > Item1);
+    return true;
+}
+
+
+// 8.
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+// A:
+  function greet(Name) {
+    return ("Hello, " + Name);
+  }
+  greet();
+
+// 9.
+// Write a function called `madlib` that takes 4 or more parameters (words).
+// The function should insert the words into a pre-defined sentence.
+// Finally the function should return that sentence.
+// Note: When I say words and sentence I mean strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+// A:
+  function madLib() {
+    var person = "John";
+    var place = "Vegas";
+    var verb = "danced";
+    var adjective = "pretty";
+
+    return (person + ' went to' + place + ' and' + verb + ' with a' + adjective + ' car.');
+  }
+ madLib();
